6f9d5a1e18a6d959edc2febbda8b6b5b8df86bad
nardtree <gim.kobayashi@gmail.com>
2018-02-15 15:39:37 +0900

    a

--- 10-sanitize.py
+++ 10-sanitize.py
@@ -6,27 +6,42 @@ import re
 
 import MeCab
 
-me = MeCab.Tagger('-Owakati')
+import concurrent.futures
 
 bake = '‚'
+
+def _map(arg):
+  me = MeCab.Tagger('-Owakati')
+  key, names = arg
+
+  fp = open(f'courpus/{key:09d}', 'w')
+  for name in names:
+    try:
+      obj = pickle.load(open(name, 'rb'))
+    except Exception as ex:
+      continue
+    cat = obj['href'].split('/')[-2]
+    mes = obj['msg'].replace('\n', '')
+    if bake in mes:
+      continue
+    if index%1 == 0:
+      try:
+        print(cat, mes)
+      except Exception as ex:
+        continue
+      try:
+        fp.write( f'{cat}\t{mes}\n' )
+      except Exception as ex:
+        print(ex)
+        continue
+
+args = {}
 for index, name in enumerate(glob.glob('../scraping-designs/5ch-scrape/messages/*')):
-  #print(name)
-  try:
-    obj = pickle.load(open(name, 'rb'))
-  except Exception as ex:
-    continue
-  cat = obj['href'].split('/')[-2]
-  mes = obj['msg'].replace('\n', '')
-  if bake in mes:
-    continue
-  if index%10 == 0:
-    try:
-      print(cat, mes)
-    except Exception as ex:
-      continue
-  with open(f'courpus/{cat}', 'a+') as fp:
-    #print(mes)
-    try:
-      fp.write( mes + '\n' )
-    except Exception as ex:
-      continue
+  key = index%12
+  if args.get(key) is None:
+    args[key] = []
+  args[key].append(name)
+args = [(key,names) for key, names in args.items()]
+#_map(args[0])
+with concurrent.futures.ProcessPoolExecutor(max_workers=12) as exe:
+  exe.map(_map, args)
--- 20-wakati.py
+++ 20-wakati.py
@@ -6,18 +6,22 @@ import pickle
 
 m = MeCab.Tagger('-Ochasen')
 
-for name in glob.glob('./courpus/*'):
-  tag = name.split('/').pop()
-  
-  buff = []
+tag_feats = {}
+for name in glob.glob('courpus/*'):
   for index, line in enumerate(open(name)):
-    line = line.strip()
+    try:
+      tag, line = line.strip().split('\t')
+    except Exception as ex:
+      print(ex)
+      continue
     feat = [x.split('\t').pop(0) for x in filter(lambda x:'名詞-代名詞-一般' in x or '名詞-一般' in x, m.parse(line).strip().split('\n'))]
     if feat == []:
       continue
     if index%5000 == 0:
       print(feat)
       print(line)
-    buff.append(feat)
+    if tag_feats.get(tag) is None:
+      tag_feats[tag] = []
+    tag_feats[tag].append( feat )
 
-  pickle.dump(buff, open(f'wakati/{tag}', 'wb'))  
+pickle.dump(tag_feats, open(f'wakati/tag_feats.pkl', 'wb'))  
